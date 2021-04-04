751f6b7d643501ec13a53addc9bb3e4f163ba2db
Khalil Ghanem <khalil_ghanem7@yahoo.com>
2021-04-04 16:22:37 +0300

    adding conlog

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 'use strict';
 let userName=prompt('what is your name?')
+console.log(typeof userName);
 let userAge=prompt('what is your age?')
+console.log(typeof userAge);
 let userPlayer=prompt('who is your favourite Player?')
-let usreClube=prompt('which team do you support')
-alert('Welcome to my site '+userName+', your age is: '+userAge+', your favourite Player is: '+userPlayer+', your team is: '+usreClube );
+console.log(typeof userPlayer);
+let usreClub=prompt('which team do you support')
+console.log(typeof usreClub);
+alert('Welcome to my site '+userName+', your age is: '+userAge+', your favourite Player is: '+userPlayer+', your team is: '+usreClub );
