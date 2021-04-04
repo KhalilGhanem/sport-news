146ad1fb9139d9fd6adaaa2cad62ee02d2dbd588
Khalil Ghanem <khalil_ghanem7@yahoo.com>
2021-04-04 16:12:05 +0300

    adding another prompt

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 'use strict';
 let userName=prompt('what is your name?')
+let userAge=prompt('what is your age?')
 let userPlayer=prompt('who is your favourite Player?')
 let usreClube=prompt('which team do you support')
-alert('Welcome to my site '+userName+' your favourite Player is: '+userPlayer+' your team is: '+usreClube );
+alert('Welcome to my site '+userName+', your age is: '+userAge+', your favourite Player is: '+userPlayer+', your team is: '+usreClube );
