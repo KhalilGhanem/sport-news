c594a36f65cb08292eadee98a1ab0212e3d20a99
Khalil Ghanem <khalil_ghanem7@yahoo.com>
2021-04-04 15:41:24 +0300

    adding all the files

---
+++ js/script.js
@@ -0,0 +1,5 @@
+'use strict';
+let userName=prompt('what is your name?')
+let userPlayer=prompt('who is your favourite Player?')
+let usreClube=prompt('which team do you support')
+alert('Welcome to my site '+userName+' your favourite Player is: '+userPlayer+' your team is: '+usreClube );
